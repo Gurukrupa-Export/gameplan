fa8b5397c51f2053e2c2b35e785e32af5dcaaab8
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2023-07-24 09:57:45 +0530

    Update install.py
    Commented check_frappe_version():

--- gameplan/install.py
+++ gameplan/install.py
@@ -10,13 +10,13 @@ def before_install():
 def after_install():
 	download_rembg_model()
 
-def check_frappe_version():
-	from semantic_version import Version
-	from frappe import __version__ as frappe_version
-
-	if Version(frappe_version) < Version('15.0.0'):
-		raise SystemExit('Gameplan requires Frappe Framework version 15 or above')
-
-def download_rembg_model():
-	from rembg import new_session
-	new_session()
+#def check_frappe_version():
+#	from semantic_version import Version
+#	from frappe import __version__ as frappe_version
+#
+#	if Version(frappe_version) < Version('15.0.0'):
+#		raise SystemExit('Gameplan requires Frappe Framework version 15 or above')
+#
+#def download_rembg_model():
+#	from rembg import new_session
+#	new_session()
